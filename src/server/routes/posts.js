c17b63f17fcbee54e49d1e35c734daf3ce9eeda3
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-03-29 22:53:04 +0100

    Installed babel-transform-runtime to handle async, set up working post logic in server/routes/posts

--- src/server/routes/posts.js
+++ src/server/routes/posts.js
@@ -1,13 +1,23 @@
 import express from 'express'
 const postRouter = express.Router();
-// import PostModel from '../../models/posts'
+import PostModel from '../../models/postModel'
 
 postRouter.get('/', (req, res) => {
   res.send('Working');
 })
 
-postRouter.post('/', (req, res) => {
-  console.log(req.body);
+postRouter.post('/', async (req, res) => {
+  const post = new PostModel({
+    title: req.body.title,
+    description: req.body.description
+  })
+
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
 export default postRouter;
