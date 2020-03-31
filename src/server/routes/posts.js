7463ca7a71dcc66b2fc06c14cf8745be3f278e90
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-03-31 22:15:25 +0100

    Adding in get/delete/patch ub route

--- src/server/routes/posts.js
+++ src/server/routes/posts.js
@@ -2,8 +2,13 @@ import express from 'express'
 const postRouter = express.Router();
 import PostModel from '../../models/postModel'
 
-postRouter.get('/', (req, res) => {
-  res.send('Working');
+postRouter.get('/', async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
 postRouter.post('/', async (req, res) => {
@@ -15,6 +20,40 @@ postRouter.post('/', async (req, res) => {
   try {
     const savedPost = await post.save();
     res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+postRouter.get('/:postId', async (req, res) => {
+  // console.log(req.params.postId);
+  try {
+    const post = await PostModel.findById(req.params.postId);
+    res.json(post);
+  } catch (err) {
+    res.json({ message: err });
+  }
+})
+
+postRouter.delete('/:postId', async (req, res) => {
+  try {
+    const removedPost = await PostModel.remove({ _id: req.params.postId });
+    res.json(removedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
+})
+
+postRouter.patch(':/postId', async (req, res) => {
+  try {
+    const changedPost = await PostModel.updateOne(
+      { _id: req.params.postId },
+      { $set: {
+          title: req.body.title
+        }
+      }
+    );
+    res.json(changedPost);
   } catch (err) {
     res.json({ message: err })
   }
